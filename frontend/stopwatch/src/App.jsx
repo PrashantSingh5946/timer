04aa0ce70cc8aa6703b66d81673346cc7246d4e7
Prashant Singh <octanesingh@gmail.com>
2022-08-01 07:34:12 +0530

    Base layout

--- frontend/stopwatch/src/App.jsx
+++ frontend/stopwatch/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  const [isTicking, setIsTicking] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      isTicking ? setElapsedTime((elapsedTime) => elapsedTime + 1000) : null;
+    }, 1000);
+  }, []);
 
   return (
-    <div>
+    <div className="container">
+      {/* Toggle Bar */}
+      <div>
+        <button
+          onClick={() => {
+            setIsTicking((isTicking) => !isTicking);
+          }}
+        >
+          Play/Pause
+        </button>
+        <button onClick={() => {}}>Reset</button>
+      </div>
+
+      <div>{elapsedTime}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
